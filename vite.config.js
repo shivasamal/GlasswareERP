0b767ce248f4223123289043bd483338c06d9202
shivasamal <shivasamala03@gmail.com>
2025-11-26 20:51:21 +0530

    new build files

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/GlasswareERP/',
-  build: { outDir: 'docs' },
+  build: { outDir: 'dist' },
   plugins: [react()],
   server: {
     port: 3000,
